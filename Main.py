b28574cdcd01b6926467559fcbccc4c3312cd7fc
karthikReddy-22 <110010639+karthikReddy-22@users.noreply.github.com>
2022-08-30 00:30:05 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -58,3 +58,22 @@ for item in input().split(','):
         data.append([])
     else:
         data.append([int(item)])
+obj = MyCircularQueue(data[0][0])
+result = []
+for i in range(len(operations)):
+    if i == 0:
+        result.append(None)
+    elif operations[i] == "enqueue":
+        result.append(obj.enqueue(data[i][0]))
+    elif operations[i] == "get_rear":
+        result.append(obj.get_rear())
+    elif operations[i] == "get_front":
+        result.append(obj.get_front())
+    elif operations[i] == "dequeue":
+        result.append(obj.dequeue())
+    elif operations[i] == "is_full":
+        result.append(obj.is_full())
+    elif operations[i] == "is_empty":
+        result.append(obj.is_empty())
+
+print(result)
